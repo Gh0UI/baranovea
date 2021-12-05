931e1b21a3af82dc538735f8fd905979c7c75d5f
Baranovea <zhenil00@mail.com>
2021-12-05 23:57:41 +0300

    add fib

---
+++ lesson5/fib.py
@@ -0,0 +1,15 @@
+def fibonacci(n):
+    cur = 1
+    if n > 2:
+        cur = fibonacci(n-1) + fibonacci(n-2)
+    return cur
+
+while True:
+        try:
+            num = int(input("Введите число\n"))
+            break                    
+        except ValueError:
+            print("Вы ввели не число. Повторите ввод")
+
+res=fibonacci(num)
+print(f'{num} элемент последовательности равен {res}')
