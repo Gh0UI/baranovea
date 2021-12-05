05dceabd5bf1f1a280ef6c9ee17e72ccf07ead8c
Baranovea <zhenil00@mail.com>
2021-12-05 23:27:59 +0300

    add password & sum

---
+++ lesson5/sum.py
@@ -0,0 +1,21 @@
+list1=[]
+num=1
+print("Введите число:\n0-выход\n")
+while num!=0:
+        try:
+            num = int(input())
+            if num==0: break
+            list1.append(num)
+        except ValueError:
+            print("Вы ввели не число. Повторите ввод")
+
+
+def sum(list1):
+    i=0
+    sum=0
+    while i < len(list1):       
+        sum+=list1[i]
+        i += 1
+    print('Cумма числел равна:', sum)
+
+sum((list1))
